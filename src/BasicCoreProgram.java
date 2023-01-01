463b2fb1041448a07fe27ce04cc76fd67cb78704
Prashant Deepake <deepakeprashant1@gmail.com>
2023-01-01 22:48:03 +0530

    ADDED: LEAP YEAR PROGRAM

--- src/BasicCoreProgram.java
+++ src/BasicCoreProgram.java
@@ -2,9 +2,28 @@ import java.util.Scanner;
 
 public class BasicCoreProgram {
     static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("WELCOME COIN PERCENTAGE");
-        flipCoinPercentage();
+        while (true) {
+            System.out.println("WELCOME TO BASIC CORE PROGRAM");
+            System.out.println("1] FLIP COIN % TAIL/HEAD");
+            System.out.println("2] LEAP YEAR PROGRAM");
+            System.out.println("3] EXIT PROGRAM");
+            System.out.println("ENTER YOUR CHOICE");
+            switch (scan.nextInt()) {
+                case 1:
+                    flipCoinPercentage();
+                    break;
+                case 2:
+                    leapYearCheck();
+                    break;
+                case 3:
+                    System.out.println("THANK YOU ..!");
+                    System.exit(0);
+                default:
+                    System.out.println("WRONG CHOICE TRY AGAIN");
+            }
+        }
     }
 
     private static void flipCoinPercentage() {
@@ -24,5 +43,15 @@ public class BasicCoreProgram {
         float Tail_Percentage = (Tail_Counter * 100) / Through;
         System.out.println("HEAD PERCENTAGE = " + Head_Percentage + "%");
         System.out.println("TAIL PERCENTAGE = " + Tail_Percentage + "%");
+    }
+    private static void leapYearCheck() {
+        System.out.println("WELCOME LEAP YEAR PROGRAM");
+        System.out.println("ENTER 2000 To 2022 ANY YEAR");
+        int Year = scan.nextInt();
+        if (Year % 4 == 0) {
+            System.out.println(Year + " IS A LEAP YEAR");
+        } else {
+            System.out.println(Year + " IS NOT LEAP YEAR");
+        }
     }
 }
